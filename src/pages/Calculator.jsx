e881ad2b502217b9e4039edc21e50e03c837a868
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2022-01-05 23:08:36 +0700

    Feat: Added useState hooks to perform calculate BMI

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,17 +1,38 @@
 import '../styles/Calculator.css';
+import React, { useState } from 'react';
 
 export default function Calculator() {
+	const [weight, setWeight] = useState(60);
+	const [height, setHeight] = useState(160);
+	const [bmiResult, setBmiResult] = useState(0.0);
+	const [bmiMsg, setBmiMsg] = useState('Health Body');
+
+	const handleWeightValueChange = (event) => setWeight(event.target.value);
+	const handleHeightValueChange = (event) => setHeight(event.target.value);
+
+	const calculateBmi = () => {
+		let bmi = (weight / Math.pow(height / 100, 2)).toFixed(1);
+		setBmiResult(bmi);
+		if (bmi < 18.5) {
+			setBmiMsg('Underweight');
+		} else if (bmi >= 18.5 && bmi <= 24.9) {
+			setBmiMsg('Normal Weight');
+		} else if (bmi >= 25 && bmi <= 29.9) {
+			setBmiMsg('Overweight');
+		} else if (bmi >= 30) {
+			setBmiMsg('Obesity');
+		}
+	};
+
 	return (
 		<div className='calculatorContent'>
-			{/* isi disini ya untuk Calculator */}
 			<h1>Calculate your Body Mass Index</h1>
-			<div class='row'>
+			<div className='row'>
 				<div className='col result'>
-					<div className='value'>0.0</div>
-					<div className='category'>
-						Let's check <br />
-						your Body
+					<div className='value'>
+						{bmiResult}
 					</div>
+					<div className='category'>{bmiMsg}</div>
 				</div>
 				<div className='col inputContainer'>
 					<div className='inputGender'>
@@ -28,24 +49,24 @@ export default function Calculator() {
 								type='range'
 								min='0'
 								max='250'
-								value='160'
+								value={height}
 								id='height'
-								oninput='updateCalculate()'
+								onChange={handleHeightValueChange}
 							/>
-							<span id='heightValue'>160cm</span>
+							<span id='heightValue'>{`${height}cm`}</span>
 						</div>
 						<div className='weightInput'>
 							<input
 								type='range'
 								min='0'
 								max='300'
-								value='60'
+								value={weight}
 								id='weight'
-								oninput='updateCalculate()'
+								onChange={handleWeightValueChange}
 							/>
-							<span id='weightValue'>60kg</span>
+							<span id='weightValue'>{`${weight}kg`}</span>
 						</div>
-						<button onclick='calculateBMI()'>Calculate</button>
+						<button onClick={calculateBmi}>Calculate</button>
 					</div>
 				</div>
 			</div>
